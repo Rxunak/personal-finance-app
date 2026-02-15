246057430753618e61e98e33cd6601f5f31cda9a
Rxunak <raunakuk2016@gmail.com>
2026-02-15 10:40:32 +0000

    Full Sidebar integration with navigation completed

--- src/components/appSidebar.tsx
+++ src/components/appSidebar.tsx
@@ -2,19 +2,22 @@
 
 import {
   Sidebar,
-  SidebarContent,
   SidebarFooter,
-  SidebarGroup,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarTrigger,
 } from "../components/ui/sidebar";
 import Link from "next/link";
-import Image from "next/image";
 import { useSidebar } from "../components/ui/sidebar";
 import { usePathname } from "next/navigation";
-
+import LogoLarge from "../icons/logo-large.svg";
+import LogoSmall from "../icons/logo-small.svg";
+import IconNavBudgets from "../icons/icon-nav-budgets.svg";
+import IconNavOverview from "../icons/icon-nav-overview.svg";
+import IconNavPots from "../icons/icon-nav-pots.svg";
+import IconNavRecurringBills from "../icons/icon-nav-recurring-bills.svg";
+import IconNavTransactions from "../icons/icon-nav-transactions.svg";
 export function AppSidebar() {
   const { state } = useSidebar();
   const isCollapsed = state === "collapsed";
@@ -23,67 +26,45 @@ export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader className={isCollapsed ? "p-4 mb-3" : "p-6"}>
-        <Image
-          src={
-            isCollapsed ? "/images/logo-small.svg" : "/images/logo-large.svg"
-          }
-          width={isCollapsed ? 10 : 120}
-          height={120}
-          alt="logo"
-        />
+        {isCollapsed ? <LogoSmall /> : <LogoLarge />}
       </SidebarHeader>
       <SidebarMenu className={isCollapsed ? "gap-4" : ""}>
         <SidebarMenuButton
           className={
             isCollapsed
-              ? "p-3 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green w-10"
-              : "p-6 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green"
+              ? "group/overview p-3 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green w-10"
+              : "group/overview p-6 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green"
           }
           isActive={pathname === "/"}
         >
-          <Link href="/" className="flex gap-5">
-            <Image
-              src="/images/icon-nav-overview.svg"
-              width={18}
-              height={18}
-              alt="logo"
-            />
+          <Link href="/" className="flex gap-5 items-center">
+            <IconNavOverview className="text-gray-400 group-hover/overview:text-green group-data-[active=true]/overview:text-green transition-colors" />
             {!isCollapsed ? "Overview" : ""}
           </Link>
         </SidebarMenuButton>
         <SidebarMenuButton
           className={
             isCollapsed
-              ? "p-3 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green w-10"
-              : "p-6 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green"
+              ? "group/transactions p-3 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green w-10"
+              : "group/transactions p-6 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green"
           }
           isActive={pathname === "/transactions"}
         >
-          <Link href="/transactions" className="flex gap-5">
-            <Image
-              src="/images/icon-nav-transactions.svg"
-              width={18}
-              height={18}
-              alt="logo"
-            />
+          <Link href="/transactions" className="flex gap-5 items-center">
+            <IconNavTransactions className="text-gray-400 group-hover/transactions:text-green group-data-[active=true]/transactions:text-green transition-colors" />
             {!isCollapsed ? " Transactions" : ""}
           </Link>
         </SidebarMenuButton>
         <SidebarMenuButton
           className={
             isCollapsed
-              ? "p-3 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green w-10"
-              : "p-6 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green"
+              ? "group/budgets p-3 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green w-10"
+              : "group/budgets p-6 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green"
           }
           isActive={pathname === "/budget"}
         >
-          <Link href="/budget" className="flex gap-5">
-            <Image
-              src="/images/icon-nav-budgets.svg"
-              width={18}
-              height={18}
-              alt="logo"
-            />
+          <Link href="/budget" className="flex gap-5 items-center">
+            <IconNavBudgets className="text-gray-400 group-hover/budgets:text-green group-data-[active=true]/budgets:text-green transition-colors" />
 
             {!isCollapsed ? "Budgets" : ""}
           </Link>
@@ -91,36 +72,26 @@ export function AppSidebar() {
         <SidebarMenuButton
           className={
             isCollapsed
-              ? "p-3 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green w-10"
-              : "p-6 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green"
+              ? "group/pots p-3 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green w-10"
+              : "group/pots p-6 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green"
           }
           isActive={pathname === "/pots"}
         >
-          <Link href="/pots" className="flex gap-5">
-            <Image
-              src="/images/icon-nav-pots.svg"
-              width={18}
-              height={18}
-              alt="logo"
-            />
+          <Link href="/pots" className="flex gap-5 items-center">
+            <IconNavPots className="text-gray-400 group-hover/pots:text-green group-data-[active=true]/pots:text-green transition-colors" />
             {!isCollapsed ? "Pots" : ""}
           </Link>
         </SidebarMenuButton>
         <SidebarMenuButton
           className={
             isCollapsed
-              ? "p-3 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green w-10"
-              : "p-6 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green"
+              ? "group/recurring-bills p-3 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green w-10"
+              : "group/recurring-bills p-6 data-[active=true]:bg-white data-[active=true]:text-black data-[active=true]:border-l-4 border-green"
           }
           isActive={pathname === "/recurringBills"}
         >
-          <Link href="/recurringBills" className="flex gap-5">
-            <Image
-              src="/images/icon-recurring-bills.svg"
-              width={18}
-              height={18}
-              alt="logo"
-            />
+          <Link href="/recurringBills" className="flex gap-5 items-center">
+            <IconNavRecurringBills className="text-gray-400 group-hover/recurring-bills:text-green group-data-[active=true]/recurring-bills:text-green transition-colors" />
             {!isCollapsed ? "Recurring bills" : ""}
           </Link>
         </SidebarMenuButton>
